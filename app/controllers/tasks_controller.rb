3d5a26000074897f63613b4cddc2da68240462b9
Shona Chan <mojavealoe@gmail.com>
2023-03-07 12:37:18 +0800

    adds see details link to index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,11 @@
 class TasksController < ApplicationController
+
+  #GET /tasks
   def index
     @tasks = Task.all
   end
 
+  #GET /tasks/:id
   def show
     @task = Task.find(params[:id])
   end
