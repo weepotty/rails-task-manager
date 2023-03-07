a400d19165587923b8071acc75a0daf00e43de73
Shona Chan <mojavealoe@gmail.com>
2023-03-07 12:33:44 +0800

    creates show route, method and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/tasks', to: 'tasks#index'
+
+  # As a user, I can list tasks
+  get '/tasks', to: 'tasks#index', as: :tasks
+  # As a user, I can view the details of a task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+# As a user, I can add a new task
+# post '/tasks', to: 'tasks#create'
+
+
+
+# As a user, I can edit a task (mark as completed / update title & details)
+# As a user, I can remove a task
 end
