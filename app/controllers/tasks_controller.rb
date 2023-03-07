f5ca0da81aa6c2b86412428ceb217773490e5b9d
Shona Chan <mojavealoe@gmail.com>
2023-03-07 12:51:02 +0800

    allows user to add new task and redirects to index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,31 @@
 class TasksController < ApplicationController
-
-  #GET /tasks
+  # GET /tasks
   def index
     @tasks = Task.all
   end
 
-  #GET /tasks/:id
+  # GET /tasks/:id
   def show
     @task = Task.find(params[:id])
   end
 
-  #GET /tasks/new
+  # GET /tasks/new
   def new
     @task = Task.new
   end
 
-  #POST /tasks
+  # POST /tasks
   def create
+    Task.create!(create_params)
+    redirect_to tasks_path
+  end
 
+  private
+
+  def create_params
+    params.require(:task).permit(
+      :title,
+      :details
+    )
   end
 end
